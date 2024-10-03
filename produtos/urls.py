1d446a74b8ed4590667d8a1c5abe8d2ad072e54b
pedrostanik <pedro.ostanik@gmail.com>
2024-10-03 09:41:55 -0300

    att

--- produtos/urls.py
+++ produtos/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from produtos import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
+    path('admin/', admin.site.urls),    
     path('list_products/', views.get_products, name='list_products')
 ]
 
